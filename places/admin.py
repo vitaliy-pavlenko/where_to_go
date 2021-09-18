b0682c7dbda2e02f142f2f05d63f9bc4745c31db
Vitaliy Pavlenko <vpavlenko@iponweb.net>
2021-09-18 20:14:39 +0300

    Inline place images download in place form

--- places/admin.py
+++ places/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 from .models import Place, PlaceImage
 
-admin.site.register(Place)
-admin.site.register(PlaceImage)
+
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        PlaceImageInline
+    ]
