a8ab8503e981cf2cf53dbb7d18755b547b2f3edc
Vitaliy Pavlenko <vpavlenko@iponweb.net>
2021-09-23 14:13:30 +0300

    show image preview in inline section

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Place, PlaceImage
 
 
 class PlaceImageInline(admin.TabularInline):
     model = PlaceImage
+    readonly_fields = ['place_image_preview']
+
+    def place_image_preview(self, obj):
+        return format_html('<img src="{}" width="{}">', obj.image.url, 200)
 
 
 @admin.register(Place)
