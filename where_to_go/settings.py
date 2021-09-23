777e79d8854629f4f7fdedd8c2419807af923fc4
Vitaliy Pavlenko <vpavlenko@iponweb.net>
2021-09-23 15:51:16 +0300

    fix media paths

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join('static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIA_ROOT = os.path.join('media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
