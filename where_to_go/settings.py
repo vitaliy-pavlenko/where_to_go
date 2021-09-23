3a3bddb6a45ac4c856c3141822de69c9e7efd80b
Vitaliy Pavlenko <vpavlenko@iponweb.net>
2021-09-23 15:46:46 +0300

    update STATIC_ROOT setting

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join('static'),)
 
