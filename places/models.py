78ac2dea4ab8ff9277c3186859f62f8e27eac32c
Vitaliy Pavlenko <vpavlenko@iponweb.net>
2021-10-11 23:42:55 +0300

    add db_index for some fields

--- places/models.py
+++ places/models.py
@@ -3,7 +3,7 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField('Заголовок', max_length=200)
+    title = models.CharField('Заголовок', max_length=200, db_index=True)
     short_title = models.CharField('Краткий заголовок', max_length=200)
     short_description = models.TextField('Краткое описание', blank=True)
     long_description = HTMLField('Описание', blank=True)
@@ -18,7 +18,7 @@ class Place(models.Model):
 class PlaceImage(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images', verbose_name='Место')
     image = models.ImageField('Изображение')
-    position = models.IntegerField('Порядок следования')
+    position = models.IntegerField('Порядок следования', db_index=True)
 
     def __str__(self):
         return f'{self.position} {self.place}'
